b31bc9bfc1e45962ca18274745b3c03280cacf1a
Legalmoney <madukacharles2020@gmail.com>
2026-08-16 23:33:30 +0100

    Complete responsive navbar and cart updates

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../app/store";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+
+  const cartQuantity = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <>
-      {/* Top contact and promotional bar */}
+      {/* Contact and promotion bar */}
       <header className="topbar">
         <div className="container topbar-content">
           <div className="topbar-left">
             <span>
-              <i className="fa-solid fa-phone" aria-hidden="true"></i>
+              <i className="fa-solid fa-phone" aria-hidden="true" />
               <span>(225) 555-0118</span>
             </span>
 
             <span>
-              <i className="fa-regular fa-envelope" aria-hidden="true"></i>
+              <i className="fa-regular fa-envelope" aria-hidden="true" />
               <span>michelle.rivera@example.com</span>
             </span>
           </div>
@@ -27,31 +43,50 @@ function Navbar() {
             <span>Follow Us :</span>
 
             <a href="#" aria-label="Instagram">
-              <i className="fa-brands fa-instagram" aria-hidden="true"></i>
+              <i className="fa-brands fa-instagram" aria-hidden="true" />
             </a>
 
             <a href="#" aria-label="YouTube">
-              <i className="fa-brands fa-youtube" aria-hidden="true"></i>
+              <i className="fa-brands fa-youtube" aria-hidden="true" />
             </a>
 
             <a href="#" aria-label="Facebook">
-              <i className="fa-brands fa-facebook" aria-hidden="true"></i>
+              <i className="fa-brands fa-facebook" aria-hidden="true" />
             </a>
 
             <a href="#" aria-label="Twitter">
-              <i className="fa-brands fa-twitter" aria-hidden="true"></i>
+              <i className="fa-brands fa-twitter" aria-hidden="true" />
             </a>
           </div>
         </div>
       </header>
 
-      {/* Main site navigation */}
       <nav className="navbar">
         <div className="container navbar-content">
-          <Link to="/" className="logo">
+
+          {/* Mobile menu toggle */}
+          <button
+            type="button"
+            className="mobile-menu-toggle"
+            onClick={() => setIsMobileMenuOpen((open) => !open)}
+            aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
+            aria-expanded={isMobileMenuOpen}
+          >
+            <i
+              className={
+                isMobileMenuOpen
+                  ? "fa-solid fa-xmark"
+                  : "fa-solid fa-bars"
+              }
+              aria-hidden="true"
+            />
+          </button>
+
+          <Link to="/" className="logo" onClick={closeMobileMenu}>
             Bandage
           </Link>
 
+          {/* Desktop navigation */}
           <ul className="nav-links">
             <li>
               <Link to="/">Home</Link>
@@ -63,7 +98,7 @@ function Navbar() {
                 <i
                   className="fa-solid fa-chevron-down"
                   aria-hidden="true"
-                ></i>
+                />
               </Link>
             </li>
 
@@ -84,9 +119,10 @@ function Navbar() {
             </li>
           </ul>
 
+          {/* Desktop actions */}
           <div className="nav-actions">
             <Link to="/" className="login-link">
-              <i className="fa-regular fa-user" aria-hidden="true"></i>
+              <i className="fa-regular fa-user" aria-hidden="true" />
               <span>Login / Register</span>
             </Link>
 
@@ -94,15 +130,19 @@ function Navbar() {
               <i
                 className="fa-solid fa-magnifying-glass"
                 aria-hidden="true"
-              ></i>
+              />
             </button>
 
-            <Link to="/cart" className="nav-icon cart-link" aria-label="Cart">
+            <Link
+              to="/cart"
+              className="nav-icon cart-link"
+              aria-label="Cart"
+            >
               <i
                 className="fa-solid fa-cart-shopping"
                 aria-hidden="true"
-              ></i>
-              <span className="nav-count">0</span>
+              />
+              <span className="nav-count">{cartQuantity}</span>
             </Link>
 
             <button
@@ -110,10 +150,90 @@ function Navbar() {
               className="nav-icon wishlist-link"
               aria-label="Wishlist"
             >
-              <i className="fa-regular fa-heart" aria-hidden="true"></i>
+              <i className="fa-regular fa-heart" aria-hidden="true" />
               <span className="nav-count">1</span>
             </button>
           </div>
+
+          {/* Mobile actions */}
+          <div className="mobile-actions">
+            <button
+              type="button"
+              className="mobile-nav-icon"
+              aria-label="Search"
+            >
+              <i
+                className="fa-solid fa-magnifying-glass"
+                aria-hidden="true"
+              />
+            </button>
+
+            <Link
+              to="/cart"
+              className="mobile-nav-icon"
+              aria-label="Cart"
+            >
+              <i
+                className="fa-solid fa-cart-shopping"
+                aria-hidden="true"
+              />
+              <span>{cartQuantity}</span>
+            </Link>
+
+            <button
+              type="button"
+              className="mobile-nav-icon"
+              aria-label="Wishlist"
+            >
+              <i className="fa-regular fa-heart" aria-hidden="true" />
+              <span>1</span>
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile navigation */}
+        <div
+          className={`mobile-menu ${
+            isMobileMenuOpen ? "mobile-menu--open" : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Shop
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Pages
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
